60d9cb9ec0d6684199b2380732c68428ece4506c
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-01-11 16:33:01 +0300

    completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import blogData from "data/blog.js"
+// function Header (name) {
+
+
+// }
+    const Header = ({name}) =>{
+        return (
+            <header> <h1>{name}</h1></header>
+        )
+    }
+   
+export default Header
--- src/data/blog.js
+++ src/data/blog.js
@@ -29,3 +29,6 @@ const blogData = {
 };
 
 export default blogData;
+
+
+
